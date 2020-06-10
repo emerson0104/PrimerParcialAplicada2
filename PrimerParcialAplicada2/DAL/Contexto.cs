6468627b7f3cb104e50f6d7d6e23548722f1e671
Emerson Del Orbe Candelario <emerson_delorbe@ucne.edu.do>
2020-06-10 13:32:11 -0400

    Add project files.

---
+++ PrimerParcialAplicada2/DAL/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerParcialAplicada2.DAL
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"DATA\Parcial1.db");
+        }
+    }
+}
